28e54c5d5e7778ff4c2d5619f53a9d6e7a7de3d9
“Scott <stevensscott29@gmail.com>
2023-07-17 22:00:09 -0400

    added cors info

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+gem "rack-cors"
 
 ruby "3.1.3"
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,50 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "test", password: "test", except: [:index, :show]
-
+  # http_basic_authenticate_with name: "test", password: "test", except: [:index, :show]
+  
   def index
     @articles = Article.all
-  end
 
+    #  respond_to code block will allow you to render the html if you're in your browser visiting localhost:3000, OR render json if you're making a web request with '.json' at the end 
+    respond_to do |format|
+      format.html { @articles }
+      # format.json { render json: @articles.as_json }
+      format.json { render json: @articles.as_json(include:[:comments]) }
+    end
+  end
+  # This won't work until I create a seperate show render page I think since I only have the content page it shows the index as well as the show
   def show
-    @article = Article.find(params[:id])
+     @article = Article.find(params[:id])
+    # respond_to do |format|
+    #   format.html { @article }
+    #   format.json { render json: @article.as_json }
+    # end
   end
 
   def new
     @article = Article.new
   end
 
-  def create
+  def create  
+    # I'm now using a new private function instead of explicitly writing out each article attribute per the rails tutorial.
     @article = Article.new(article_params)
+   
+   
 
-    if @article.save
-      redirect_to @article
-    else
-      render :new, status: :unprocessable_entity
+    if @article.save     
+      # the respond_to allows you to have a Rails frontend (html) and a React frontend (json)
+      respond_to do |format|
+        format.html do
+          p "In format html"
+          redirect_to @article
+        end
+        
+        format.json do  
+          p "In format json"       
+          render json: @article.as_json
+        end
+      end
+    else        
+      render :json => { :errors => @article.errors.full_messages }
     end
   end
 
@@ -29,9 +54,12 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
+    p "article #{@article.to_json}"
 
     if @article.update(article_params)
-      redirect_to @article
+      p "article params #{article_params.to_json}"
+      render json: @article.as_json
+      # redirect_to @article
     else
       render :edit, status: :unprocessable_entity
     end
@@ -44,6 +72,7 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+  
   private
     def article_params
       params.require(:article).permit(:title, :body, :status)
